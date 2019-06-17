70f15ef9d8e802ac4a6046695a1d0d632a67b69f
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-17 22:01:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,12 +10,12 @@ class Scraper
 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
-      # doc.css(".post").each do |post|
-      # course = Course.new
-      # course.title = post.css("h2").text
-      # course.schedule = post.css(".date").text
-      # course.description = post.css("p").text
-    end
+    # doc.css(".post").each do |post|
+    # course = Course.new
+    # course.title = post.css("h2").text
+    # course.schedule = post.css(".date").text
+    # course.description = post.css("p").text
+  end
 
   def get_courses
     self.get_page.css(".post")
@@ -23,14 +23,14 @@ class Scraper
 
   def make_courses
     self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
   end
 
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -39,7 +39,7 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
-end
+  end
 end
 
 Scraper.new.get_page
